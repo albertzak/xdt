6cc8afd651eb461a0aeb4b869f44d82f6756077b
Albert Zak <me@albertzak.com>
2016-01-01 10:11:41 +0100

    💆 Add basic parsing of patients records

--- main.js
+++ main.js
@@ -3,21 +3,84 @@
 var fs = require('fs');
 var fsAccess = require('fs-access');
 var iconv = require('iconv-lite');
+var _ = require('lodash');
+var moment = require('moment');
 
 var Xdt = function(options) {
+
+    var RX = new RegExp(
+       '^\\r?\\n?'   // ignore leading newlines
+     + '(\\d{3})'    // line length
+     + '(\\d{4})'    // field id
+     + '(.*?)'       // field data
+     + '\\r?\\n?$',  // match data until end of line or EOF
+       'mg');         // match each line
+
+
+    var FIELDS = {
+      id: '3000',
+      lastName: '3101',
+      firstName: '3102',
+      birthday: '3103',
+      gender: '3110'
+    };
 
     if (typeof options === 'undefined')
       options = {}
     if ( ! options.encoding)
       options.encoding = 'ISO-8859-1';
 
+    this.options = options;
+
     this.open = function(path, callback) {
       var _this = this;
 
       fsAccess(path, function(err) {
         if (err) return callback(err, _this);
-        
+        fs.readFile(path, function (err, buffer) {
+          if (err) return callback(err, _this);
+
+          _this.raw = iconv.decode(buffer, options.encoding);
+          _this.parsed = _this.parse();
+          _this.patient = _this.patient();
+          return callback(null, _this);
+        });
       });
+    };
+
+    this.parse = function() {
+      var match;
+      var matches = [];
+      while ((match = RX.exec(this.raw)) !== null) {
+        match = {
+          length: parseInt(match[1]),
+          field: match[2],
+          value: match[3]
+        };
+
+        matches.push(match);
+      }
+
+      return matches;
+    };
+
+    this.patient = function(parsed) {
+      return {
+        id: this.first(FIELDS.id),
+        firstName: this.first(FIELDS.firstName),
+        lastName: this.first(FIELDS.lastName),
+        birthday: moment(this.first(FIELDS.birthday), 'DDMMYYYY').toDate(),
+        gender: this.first(FIELDS.gender)
+      }
+    };
+
+    this.first = function(field) {
+      var field;
+      field = _.find(this.parsed, function(p) {
+        return p.field === field;
+      });
+
+      return field && field.value;
     };
 
 
--- test/test.js
+++ test/test.js
@@ -1,5 +1,8 @@
 var Xdt = require('../main.js');
 var assert = require('assert');
+var moment = require('moment');
+
+var fixture = 'test/fixtures/gdt2_1-1.txt';
 
 describe('Xdt', function() {
   describe('.open', function () {
@@ -11,12 +14,60 @@ describe('Xdt', function() {
 
     it('should check for file permissions', function(done) {
       assert.doesNotThrow(function() {
-        xdt.open('file-which-does-not-exist', function(err, data) {
-          assert(err.code === 'ENOENT');
+        xdt.open('file-which-does-not-exist', function(err) {
+          assert.equal(err.code, 'ENOENT');
           done();
           return;
         });
       });
+    });
+
+    it('should read file', function(done) {
+      xdt.open(fixture, function(err) {
+        assert.equal(err, null);
+        done();
+        return;
+      });
+    });
+  });
+
+  describe('.parse', function () {
+    var xdt = null;
+    var parsed = null;
+
+    beforeEach(function(done) {
+      xdt = new Xdt();
+      xdt.open(fixture, function(err, xdt) {
+        parsed = xdt.parse(xdt.raw);
+        done();
+        return;
+      });
+    });
+
+    it('should parse raw fields', function() {
+      assert.equal(parsed.length, 12);
+      assert.equal(parsed[0].field, '8000');
+    });
+  });
+
+  describe('.patient', function () {
+    var xdt = null;
+
+    beforeEach(function(done) {
+      xdt = new Xdt();
+      xdt.open(fixture, function(err, obj) {
+        xdt = obj;
+        done();
+        return;
+      });
+    });
+
+    it('should parse raw fields', function() {
+      assert.equal(xdt.patient.id, '02345');
+      assert.equal(xdt.patient.firstName, 'Franz');
+      assert.equal(xdt.patient.lastName, 'Mustermann');
+      assert.equal(xdt.patient.gender, '1');
+      assert.equal(xdt.patient.birthday.getTime(), moment('1945-10-01').toDate().getTime());
     });
   });
 
